53d7404da761cdc37ef2120ec2ef4db855785f42
keychera <kyogatama@gmail.com>
2021-09-21 12:47:01 +0700

    stop using lombok for generation purposes

--- src/main/java/self/chera/grpc/RPCException.java
+++ src/main/java/self/chera/grpc/RPCException.java
@@ -2,13 +2,10 @@ package self.chera.grpc;
 
 import com.google.protobuf.GeneratedMessageV3;
 import io.grpc.StatusRuntimeException;
-import lombok.Getter;
 
 public class RPCException extends StatusRuntimeException {
-    @Getter
-    String stepName;
-    @Getter
-    GeneratedMessageV3 request;
+    public final String stepName;
+    public final GeneratedMessageV3 request;
 
     public RPCException(StatusRuntimeException sre, String stepName, GeneratedMessageV3 req) {
         super(sre.getStatus(), sre.getTrailers());
